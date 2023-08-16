a2ebc6e8eeb8d8267277c6d2ea469a118035c329
j4ck3 <jacobhallgren@live.se>
2023-08-16 04:48:41 +0200

    error handling & image link fix

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -4,16 +4,18 @@ const check = require('../services/scrapeService');
 
 router.get('/check', async (req, res) => {
     try {
-        const product = await check(req.body.url);
+        const product = await check(req.query.target);
 
-        if (product.stock == 'i lager') 
+        if (product) 
             res.status(200).json({ product });
-
-        res.status(200).json({ product} );
+        else {
+            res.status(404).json({ message: 'Could not find the product' });
+        }
+     
         
     } catch (err) {
         console.error('Error checking stock:', err);
-        res.status(500).json({ message: 'An error occurred while checking the product' });
+        res.status(500).json({ message: '500: An error occurred while checking the product' });
     }
 });
 
--- src/services/scrapeService.js
+++ src/services/scrapeService.js
@@ -5,30 +5,31 @@ const sendEmail = require('../services/emailService')
 
 const check = async (url) => {
     try {
-        const product = { stock: '', image: '', price: '' }
+        const product = { title: '', stock: '', image: '', price: '', url: url }
         const response = await axios.get(url);
         const $ = cheerio.load(response.data);
 
+
         const StockStatusText = $('.Text_Lagerstatus').text().toLocaleLowerCase().trim();
+        const title = $('#ArtikelnamnFalt').text();
         const priceDiscount = $('.PrisREA').text().replace('kr', '').trim();
         const priceOrg = $('.PrisORD').text().replace('kr', '').trim();
         const image = $('#Zoomer').attr('href');
 
 
+        product.title = title
         product.price = priceDiscount !== null && priceDiscount !== undefined ? priceDiscount : priceOrg;
         product.stock = StockStatusText
-        product.image = `https://www.maxgaming.se/${image}`
+        product.image = `https://www.maxgaming.se${image}`
 
 
-        if (StockStatusText == 'i lager') {
+        if (StockStatusText == 'i lager') 
             await sendEmail(product)
-            return product
-        }
         
         return product
     }
     catch (err) {
-        console.error('Failed to send the mail:', err);
+        console.error('Failed to scrape the product:', err);
         return null
     }
 }
