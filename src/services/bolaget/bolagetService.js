39b635294cb7238811f5783ac77185c6d2e1bab5
j4ck3 <jacobhallgren@live.se>
2023-09-22 02:30:19 +0200

    query elements with n-th child

--- src/services/bolaget/bolagetService.js
+++ src/services/bolaget/bolagetService.js
@@ -1,89 +1,87 @@
 const puppeteer = require('puppeteer-extra');
 const StealthPlugin = require('puppeteer-extra-plugin-stealth');
-puppeteer.use(StealthPlugin())
-const { executablePath } = require('puppeteer')
+puppeteer.use(StealthPlugin());
+const { executablePath } = require('puppeteer');
 
 const getDrink = async (url, area) => {
-    const browser = await puppeteer.launch({headless: "new", executablePath: executablePath()})
-    const page = await browser.newPage()
+  const browser = await puppeteer.launch({
+    headless: 'false',
+    executablePath: executablePath(),
+  });
+  const page = await browser.newPage();
 
-    //set age consent cookie
-    const decodedValue = '{"state":{"verified":true},"version":0}';
-    await page.setCookie({
-      name: 'systembolaget.age',
-      value: decodedValue,
-      domain: '.systembolaget.se'
-    });
+  //set age consent cookie
+  const decodedValue = '{"state":{"verified":true},"version":0}';
+  await page.setCookie({
+    name: 'systembolaget.age',
+    value: decodedValue,
+    domain: '.systembolaget.se',
+  });
 
-    await page.goto(url, {
-      waitUntil: 'domcontentloaded',
-    })
+  await page.goto(url, {
+    waitUntil: 'domcontentloaded',
+  });
 
-    //find the show stored button an click it
-    const button = await page.$('.css-103vv0f');
+  //find the show stores button an click it
+  const button = await page.waitForSelector('.css-103vv0f');
 
   if (button) {
-
     await button.click();
 
-    // Listen for the dialog event and dissmiss it.
+    // Listen for dialog event and dissmiss it.
     page.on('dialog', async (dialog) => {
-    await dialog.dismiss();
+      await dialog.dismiss();
     });
 
     //Select hela sverige on the <select> element
     const selectElement = await page.$('.css-dz8wl');
     if (selectElement) {
-    const valueToSelect = 'sweden';
-    await page.select('select', valueToSelect);
+      const valueToSelect = 'sweden';
+      await page.select('select', valueToSelect);
     } else {
       console.log('Select element not found');
-      browser.close()
+      browser.close();
+      return;
     }
   } else {
     console.log('Button not found');
-    browser.close()
-    return
+    browser.close();
+    return;
   }
 
-//Wait for stores then return stores
-
+  //array from all the stores
   await page.waitForSelector('.css-4od5c4');
 
-  const storesData = await page.evaluate(async () => {
+  try {
+    const storesData = await page.evaluate(async () => {
+      const stores = Array.from(document.querySelectorAll('.css-4od5c4'));
 
-    const convertCount = (amount) => {
-      return parseFloat(amount.replace('st', '').trim());
+      return stores.map((e) => {
+        const paragraphs = [...e.querySelectorAll('p')].map((e) =>
+          e.textContent.trim()
+        );
+        return {
+          address: paragraphs[0] || 'no address',
+          city: paragraphs[1] ? paragraphs[1].toLowerCase().trim() : 'no city',
+          amount: paragraphs[3] || 'no amount',
+        };
+      });
+    });
+    //filter stores on area
+    if (area) {
+      const filteredStoresData = storesData.filter(
+        (store) => store.city === area.toLocaleLowerCase()
+      );
+      console.log(filteredStoresData);
+      return filteredStoresData;
+    } else {
+      return storesData;
+    }
+  } catch (e) {
+    console.error(e, 'could not get store data');
+  } finally {
+    browser.close();
   }
-
-  const stores = Array.from(document.querySelectorAll('.css-4od5c4'));
-  const data = stores.map((item) => {
-      let address = item.querySelector('.css-1tkalz1 .css-iqfm9l').textContent.trim();
-      let city = item.querySelector('.css-1tkalz1 .css-1cwtvfm').textContent.trim().toLowerCase();
-      let amountElement = item.querySelector('.css-177ui4i .css-iqfm9l');
-      let amount = amountElement ? amountElement.textContent.trim() : 'no value';
-
-      let convertedAmount = convertCount(amount);
-
-      return {
-          address,
-          city,
-          amount: convertedAmount
-      };
-  });
-    return data;
-});
-
-browser.close()
-
-//filter stores on area
-if (area) {
-    const filteredStoresData = storesData.filter(store => store.city === area.toLocaleLowerCase());
-    return filteredStoresData
-} else {
-  console.log(storesData)
-  return storesData
-}
 };
 
 module.exports = getDrink;
